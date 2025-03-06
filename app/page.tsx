69e7f213dd553a7719b65d2010a8fa5da88c0c5a
Henry Moran <virtualhenry@gmail.com>
2025-03-06 13:33:00 -0800

    Add app progress section with embedded Twitter tweets
    - Introduced new "App Progress" section to landing page
    - Embedded three Twitter tweets showcasing app development journey
    - Added motion animations for smooth section entrance
    - Included Twitter widget script for tweet rendering

--- app/page.tsx
+++ app/page.tsx
@@ -71,6 +71,85 @@ function AnimatedContent() {
           </div>
         </div>
       </motion.div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, delay: 1.2 }}
+        className="w-full text-center"
+      >
+        <h2 className="text-3xl font-bold mb-8 text-neutral-800 font-albert">
+          App Progress
+        </h2>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 1.4 }}
+          className="flex flex-col items-center gap-6"
+        >
+          {/* Most recent tweets at the top */}
+          <blockquote className="twitter-tweet" data-media-max-width="560">
+            <p lang="en" dir="ltr">
+              My calendar is my todo list
+              <br />
+              <br />
+              Blocking out time for deep work
+              <a href="https://t.co/MbNqk8U7Rf">pic.twitter.com/MbNqk8U7Rf</a>
+            </p>
+            &mdash; Henry Moran (@soyhenryxyz){" "}
+            <a href="https://twitter.com/soyhenryxyz/status/1897726452147294437?ref_src=twsrc%5Etfw">
+              March 6, 2025
+            </a>
+          </blockquote>
+          <script
+            async
+            src="https://platform.twitter.com/widgets.js"
+            charSet="utf-8"
+          ></script>
+          <blockquote className="twitter-tweet" data-media-max-width="560">
+            <p lang="en" dir="ltr">
+              First iterations of my app (Zero Proof) icon the for the sobriety
+              app I&#39;m building. A sobriety coach to help you quit drinking
+              <br />
+              <br />
+              option a: a tile from the streaks element
+              <br />
+              option b: a bit more literal
+              <br />
+              <br />
+              I&#39;m refining it but what are your initial impressions?{" "}
+              <a href="https://t.co/kqtCz7NsXS">pic.twitter.com/kqtCz7NsXS</a>
+            </p>
+            &mdash; Henry Moran (@soyhenryxyz){" "}
+            <a href="https://twitter.com/soyhenryxyz/status/1897715828671627422?ref_src=twsrc%5Etfw">
+              March 6, 2025
+            </a>
+          </blockquote>
+          <blockquote className="twitter-tweet">
+            <p lang="en" dir="ltr">
+              I&#39;m looking for feedback on this waitlist to see if it&#39;s
+              clear
+              <br />
+              <br />A sobriety ios app to help you quit / reduce consumption{" "}
+              <br />
+              <br />
+              it&#39;s for folks who want to visually track progress for sober
+              days, use data as motivation, track saved $, track eliminated
+              calories, stay accountable{" "}
+              <a href="https://t.co/I4kecTD111">pic.twitter.com/I4kecTD111</a>
+            </p>
+            &mdash; Henry Moran (@soyhenryxyz){" "}
+            <a href="https://twitter.com/soyhenryxyz/status/1895600478496637251?ref_src=twsrc%5Etfw">
+              February 28, 2025
+            </a>
+          </blockquote>
+          <script
+            async
+            src="https://platform.twitter.com/widgets.js"
+            charSet="utf-8"
+          ></script>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
